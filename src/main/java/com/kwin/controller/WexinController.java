7e5b99cb5bdc3f7582f8a19906cc3b253b88e166
Kwin <969263948@qq.com>
2019-01-09 22:27:19 +0800

    first commit.

---
+++ src/main/java/com/kwin/controller/WexinController.java
@@ -0,0 +1,25 @@
+package com.kwin.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@Slf4j
+@RequestMapping("/weixin")
+public class WexinController {
+
+    @RequestMapping("/auth")
+    public void auth(@RequestParam("code") String code) {
+        log.info("进入auth方法");
+        log.info("code={}", code);
+
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx731ec8eb23441f81&secret" +
+                "=3314b50048c547b6bdeb9dd0d54ab059&code=" + code + "&grant_type=authorization_code";
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class);
+        log.info("response={}", response);
+    }
+}
